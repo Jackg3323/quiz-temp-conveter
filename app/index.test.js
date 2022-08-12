219cb4d27fa6a8082eedb4a44e2cec008fa5a792
Jack Guthrie <jackg3323@gmail.com>
2022-08-12 14:02:41 +0100

    Update tests

--- app/index.test.js
+++ app/index.test.js
@@ -6,9 +6,19 @@ describe("tempConvert for celcius", () => {
     expect(tempConvert(50, "toFahrenheit")).toBe("122");
   });
 
-  test("converts temp to celcius", () => {
+  test("converts temp to celcius with string", () => {
     expect(tempConvert("one hundred", "toFahrenheit")).toBe("");
     expect(tempConvert("fifty", "toFahrenheit")).toBe("");
+  });
+
+  test("converts temp to celcius with no temp", () => {
+    expect(tempConvert("toFahrenheit")).toBe("");
+    expect(tempConvert("toFahrenheit")).toBe("");
+  });
+
+  test("converts temp to celcius with string temp", () => {
+    expect(tempConvert("100", "toFahrenheit")).toBe("212");
+    expect(tempConvert("50", "toFahrenheit")).toBe("122");
   });
 });
 
@@ -20,8 +30,18 @@ describe("tempConvert for Fahrenheit", () => {
 
   // second test does not round
 
-  test("converts temp to Fahrenheit", () => {
+  test("converts temp to Fahrenheit with string", () => {
     expect(tempConvert("fifty", "toCelsius")).toBe("");
     expect(tempConvert("one hundred", "toCelsius")).toBe("");
+  });
+
+  test("converts temp to Fahrenheit with no temp", () => {
+    expect(tempConvert("toCelsius")).toBe("");
+    expect(tempConvert("toCelsius")).toBe("");
+  });
+
+  test("converts temp to Fahrenheit with string temp", () => {
+    expect(tempConvert("100", "toCelsius")).toBe("10");
+    expect(tempConvert("50", "toCelsius")).toBe("38");
   });
 });
