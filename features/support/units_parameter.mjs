020664e35c5dfc56b07c496dc1405d568dda4de0
Jack Guthrie <jackg3323@gmail.com>
2022-08-15 15:42:09 +0100

    Change units from startwith to include

--- features/support/units_parameter.mjs
+++ features/support/units_parameter.mjs
@@ -4,6 +4,6 @@ defineParameterType({
   name: "units",
   regexp: /\s?C|\s?F|\s?Celsius|\s?Fahrenheit/,
   transformer(name) {
-    return name.startsWith("C") ? "toCelsius" : "toFahrenheit";
+    return name.includes("C") ? "toCelsius" : "toFahrenheit";
   },
 });
